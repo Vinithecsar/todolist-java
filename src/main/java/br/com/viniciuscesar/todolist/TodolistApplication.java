d74ca9c6b2a29830d3a3710f478a13da66971aaf
vinithecsar <viniciuscesarbrito@gmail.com>
2023-10-13 12:56:47 -0300

    Projeto Todolist finalizado

---
+++ src/main/java/br/com/viniciuscesar/todolist/TodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciuscesar.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
